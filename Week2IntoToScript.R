01858ef3d6b89a7428d309ade911b65e9478d3a9
Zsofia Adamkovics Larson <zsofia.al@campus-114-128.ucdavis.edu>
2024-10-03 13:56:24 -0700

    Week 2 in class work

---
+++ Week2IntoToScript.R
@@ -0,0 +1,27 @@
+7+11
+
+mynumber <- 6
+mynumber == 5
+mynumber != 5
+mynumber < 0
+mynumber > 0
+mynumber <= 6
+mynumber >= 7
+
+?log
+
+log_of_word <- log("nonsense")
+log_of_word
+
+log_of_negative <- log(-2)
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg * 2.2
+
+elephant2_lb > elephant1_lb
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+
+which(myelephants == max(myelephants))

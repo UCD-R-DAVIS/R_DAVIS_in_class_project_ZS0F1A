8d9f7657ed3778b63802208b3f0b00dde418d969
Zsofia Adamkovics Larson <zsofia.al@Zsofias-MacBook.local>
2024-11-13 21:26:36 -0800

    Week 7 Homework

---
+++ week_7_homework.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+gapminder <- read_csv("https://ucd-r-davis.github.io/R-DAVIS/data/gapminder.csv")
+library(ggplot2)
+
+view(gapminder)
+
+pop <- gapminder %>% 
+  select(country, year, pop, continent) %>% 
+  filter(year > 2000) %>% 
+  pivot_wider(names_from = year, values_from = pop) %>% 
+  mutate(pop_change_0207 = `2007` - `2002`)
+
+?facet_wrap
+
+pop %>% 
+  filter(continent != "Oceania") %>% 
+  ggplot(aes(x = reorder(country, pop_change_0207), y = pop_change_0207)) +
+  geom_col(aes(fill = continent)) +
+  facet_wrap(~continent, scales = "free") +
+  theme_bw() +
+  scale_fill_viridis_d() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        legend.position = "none") +
+  xlab("Country") +
+  ylab("Change in Population Between 2002 and 2007")

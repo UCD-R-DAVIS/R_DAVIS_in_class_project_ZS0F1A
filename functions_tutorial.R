bd7b7eefe42131da4282546ed82798ebb9874264
Zsofia Adamkovics Larson <zsofia.al@Zsofias-MacBook.local>
2024-11-30 21:32:03 -0800

    Second commit?

--- functions_tutorial.R
+++ functions_tutorial.R
@@ -35,6 +35,31 @@ F_to_K(212)
 #via the source function. This allows you to keep your functions separate from the analyses which use them.
 source('code/functions.R')
 
+install.packages("gapminder")
+library(gapminder)
+library(tidyverse)
+
+gapminder %>% 
+  filter(country == "Canada", year %in% c(1950:1970)) %>% 
+  summarize(mean(gdpPercap, na.rm = T))
+
+# Note: try to name arguments something that do not exist as a column name, to avoid confusing yourself and R
+avgGDP <- function(cntry, yr.range){
+  df <- gapminder %>% 
+    filter(country == cntry, year %in% yr.range)
+  mean(df$gdpPercap, na.rm = T)
+}
+
+avgGDP("Iran", 1980:1985)
+
+avgGDP("Zimbabwe", 1950:2000)
+
+#Pass by value
+#Functions in R almost always make copies of the data to operate on inside of a function body. 
+#When we modify a data frame inside the function we are modifying the copy of the gapminder dataset,
+#not the original variable we gave as the first argument. This is called “pass-by-value” and it
+#makes writing code much safer: you can always be sure that whatever changes you make within the 
+#body of the function, stay inside the body of the function.
 
 
 
